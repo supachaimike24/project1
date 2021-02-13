ed201fbc2c7181311f5425483829bb6ec6540a79
supachaimike24 <78993110+supachaimike24@users.noreply.github.com>
2021-02-13 11:50:53 +0700

    link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
 </head>
 <body>
+    <a href="/contact">11111111111111</a>
+    <a href="/about">02222222222</a>
     <h1>สวัสดีประเทศไทย</h1>
+
 </body>
 </html>
